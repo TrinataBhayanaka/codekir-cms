5e4758a67c62ef5e744a4baa67a34e3aad9092fb
Ovan Pulu <ovan89@gmail.com>
2015-08-18 13:41:42 +0700

    cache

--- applications/default/config/libs.conf.php
+++ applications/default/config/libs.conf.php
@@ -10,8 +10,8 @@ $EXCEL[0]['filetype'] = array('application/ms-excel', 'application/vnd.ms-excel'
 /* SMARTY CONFIG */
 
 $SMARTY[0]['template'] = APPPATH.'view/';
-$SMARTY[0]['cache'] = LIBS.'smarty/cache';
-$SMARTY[0]['config'] = LIBS.'smarty/configs';
-$SMARTY[0]['logs'] = CACHE;
+$SMARTY[0]['cache'] = CODEKIR_LIBS.'smarty/cache';
+$SMARTY[0]['config'] = CODEKIR_LIBS.'smarty/configs';
+$SMARTY[0]['logs'] = CODEKIR_CACHE;
 
 ?>

f6d40c7c96660c176b93743c20f0b010b11f92b8
pukiscuy <pukiscuy@gmail.com>
2015-08-20 10:37:01 +0700

    template admin

---
+++ applications/admin/controller/product.php
@@ -0,0 +1,60 @@
+<?php
+// defined ('TATARUANG') or exit ( 'Forbidden Access' );
+
+class product extends Controller {
+	
+	var $models = FALSE;
+	
+	public function __construct()
+	{
+		parent::__construct();
+
+		global $app_domain;
+		$this->loadmodule();
+		$this->view = $this->setSmarty();
+		$sessionAdmin = new Session;
+		$this->admin = $sessionAdmin->get_session();
+		// $this->validatePage();
+		$this->view->assign('app_domain',$app_domain);
+	}
+	public function loadmodule()
+	{
+		
+		$this->contentHelper = $this->loadModel('contentHelper');
+		$this->marticle = $this->loadModel('marticle');
+		$this->mquiz = $this->loadModel('mquiz');
+		$this->mcourse = $this->loadModel('mcourse');
+	}
+	
+	public function index(){
+		
+
+		return $this->loadView('product/listProduct');
+
+	}
+
+	public function formAddProduct(){
+		
+
+		return $this->loadView('product/addProduct');
+
+	}
+
+	public function formCatProduct(){
+		
+
+		return $this->loadView('product/addProduct');
+
+	}
+	public function listCatProduct(){
+		
+
+		return $this->loadView('product/listCatProduct');
+
+	}
+	
+
+	
+}
+
+?>
